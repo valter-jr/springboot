04a9107757c99ef175e938fb1ff6d834fa6aebcf
Valter Junior <valter.jrads@gmail.com>
2021-09-09 13:13:45 -0300

    Service Layer, component registration

--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.example.course.resources;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 import com.example.course.entities.User;
+import com.example.course.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Banjo", "banjo@gmail.com", "1234", "admin");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	// (value = "/{id}") "avisa" que vai receber um id como parâmetro
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){ // Para aceitar como parametro que vai chegar com a URL, coloca-se o @PathVariable
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
